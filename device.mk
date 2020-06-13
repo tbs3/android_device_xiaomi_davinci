088e4b1c48f673ad4f5ad3cbc2601173e1c8a591
Jesse Chan <jc@lineageos.org>
2020-06-13 11:22:14 +0800

    davinci: power-libperfmgr: adapt and rebrand for xiaomi
    Change-Id: I8aa9a712ac7cbc6085928f7cdb9e6ada8ed5e3fe
    Signed-off-by: DarkDampSquib <andrin.geiger1998@gmail.com>
    Signed-off-by: Marco Rizzo <marco.rizzo.00@outlook.com>

--- device.mk
+++ device.mk
@@ -294,7 +294,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.pixel-libperfmgr \
+    android.hardware.power@1.3-service.xiaomi-libperfmgr \
     powerhint.json
 
 # Product characteristics
