f279cec787868044bccb4b138d356ae359f20aac
Marco Rizzo <marco.rizzo.00@outlook.com>
2020-10-16 19:01:42 +0200

    davinci: manually call gapps for now
    Signed-off-by: Marco Rizzo <marco.rizzo.00@outlook.com>

--- device.mk
+++ device.mk
@@ -23,6 +23,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/davinci/davinci-vendor.mk)
 
+# Gapps
+$(call inherit-product-if-exists, vendor/gapps/gapps.mk)
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
