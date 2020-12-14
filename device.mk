c86aaee249bc6aefdd62166968cb5e0f692f6a9b
Marco Rizzo <marco.rizzo.00@outlook.com>
2020-12-14 10:58:32 +0000

    davinci: don't build trust HAL * this is not LOS
    Signed-off-by: Marco Rizzo <marco.rizzo.00@outlook.com>

--- device.mk
+++ device.mk
@@ -26,7 +26,7 @@ $(call inherit-product, vendor/xiaomi/davinci/davinci-vendor.mk)
 # Gapps
 ifeq ($(WITH_GAPPS), true)
     $(call inherit-product, vendor/gapps/gapps.mk)
-    DOT_BUILD_TYPE:=GAPPS
+    DOT_BUILD_TYPE:=BETA_GAPPS
 endif
 
 # Additional native libraries
@@ -377,10 +377,6 @@ PRODUCT_PACKAGES += \
 # Touchscreen
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # Uevent
 PRODUCT_COPY_FILES += \
