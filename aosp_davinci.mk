4a22e726053754e52c51520165b069b450713754
Marco Rizzo <marco.rizzo.00@outlook.com>
2021-01-24 19:44:24 +0000

    davinci: ship recorder and arcore by default in gapps builds
    Signed-off-by: Marco Rizzo <marco.rizzo.00@outlook.com>

--- aosp_davinci.mk
+++ aosp_davinci.mk
@@ -25,6 +25,8 @@ $(call inherit-product, device/xiaomi/davinci/device.mk)
 $(call inherit-product, vendor/aosp/config/common.mk)
 $(call inherit-product, vendor/aosp/config/telephony.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
 
 PRODUCT_NAME := aosp_davinci
 PRODUCT_DEVICE := davinci
